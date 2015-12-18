bcda11134c69079cf92bc844620c1c0c7dd0e775
theParsa <hakala.tuomas@hotmail.com>
2015-12-18 21:55:15 +0200

    Kaikki erikoispalat ovat kirottuja... >: )

--- Olio_jatko_harkkatyo/valmiiden_toteutus/rakentaja.cc
+++ Olio_jatko_harkkatyo/valmiiden_toteutus/rakentaja.cc
@@ -27,7 +27,7 @@ static const std::string RAAKILE = "RAAKILE";
 Rakentaja::Rakentaja(std::shared_ptr<Julkinen::Pelirajapinta> peli,
 	Julkinen::Koordinaatti const& pelialueenkoko,
 	std::string tiedosto,
-	std::shared_ptr<Naytto> naytto):
+	std::shared_ptr<Naytto> naytto) :
 	_peli(peli), _naytto(naytto), _koko(pelialueenkoko), _tiedosto(tiedosto),
 	_pelaajienmaara(2), _siemenluku(0), _pelaajialisatty(0)
 {
@@ -42,44 +42,44 @@ Rakentaja::~Rakentaja()
 void Rakentaja::lueTiedosto()
 {
 	std::ifstream tiedostovirta(_tiedosto.c_str());
-	if(!tiedostovirta)
+	if (!tiedostovirta)
 	{
 		throw Julkinen::Alustusvirhe("Annettua alustus tiedostoa ei lˆytynyt");
 	}
 
 	std::string komento = "";
 
-	while(tiedostovirta)
+	while (tiedostovirta)
 	{
 		tiedostovirta >> komento;
-		int (*pf)(int)=std::toupper;
+		int(*pf)(int) = std::toupper;
 		std::transform(komento.begin(), komento.end(), komento.begin(), pf);
 		std::string temp;
 		tiedostovirta >> temp;
-		if(komento == SIEMENLUKU)
+		if (komento == SIEMENLUKU)
 		{
 			_siemenluku = str2int(temp);
 			komento = "";
 		}
-		else if(komento == PELAAJIA)
+		else if (komento == PELAAJIA)
 		{
 			_pelaajienmaara = static_cast<unsigned int>(str2int(temp));
 			komento = "";
 		}
-		else if(komento == IHMINEN)
+		else if (komento == IHMINEN)
 		{
-                        char merkki = int2char(static_cast<int>(_pelaajialisatty));
-			PelaajaTiedot tiedot = {Julkinen::IHMINEN,
-                          merkki+static_cast<std::string>(" ")+temp, merkki, luoSijainti() };
+			char merkki = int2char(static_cast<int>(_pelaajialisatty));
+			PelaajaTiedot tiedot = { Julkinen::IHMINEN,
+				merkki + static_cast<std::string>(" ") + temp, merkki, luoSijainti() };
 			_pelaajat.push_back(tiedot);
 			komento = "";
 			_pelaajialisatty++;
 		}
-		else if(komento == TIETOKONE)
+		else if (komento == TIETOKONE)
 		{
-                        char merkki = int2char(static_cast<int>(_pelaajialisatty));
-			PelaajaTiedot tiedot = {Julkinen::TIETOKONE,
-                          merkki+static_cast<std::string>(" ")+temp, merkki, luoSijainti() };
+			char merkki = int2char(static_cast<int>(_pelaajialisatty));
+			PelaajaTiedot tiedot = { Julkinen::TIETOKONE,
+				merkki + static_cast<std::string>(" ") + temp, merkki, luoSijainti() };
 			_pelaajat.push_back(tiedot);
 			komento = "";
 			_pelaajialisatty++;
@@ -90,12 +90,12 @@ void Rakentaja::lueTiedosto()
 		}
 
 	}
-	
-	if(_pelaajienmaara != _pelaajialisatty)
+
+	if (_pelaajienmaara != _pelaajialisatty)
 	{
 		throw Julkinen::Alustusvirhe("Peliin lis‰ttyjen pelaajien m‰‰r‰ on eri kuin ilmoitettu pelaajien m‰‰r‰");
 	}
-	if(_siemenluku == 0)
+	if (_siemenluku == 0)
 	{
 		_alustaja = std::shared_ptr<Alustaja>(new Alustaja(_pelaajienmaara));
 	}
@@ -108,70 +108,57 @@ void Rakentaja::lueTiedosto()
 
 void Rakentaja::lisaaPalat()
 {
-   ESIEHTO(_alustaja != 0);
-   _peli->maaritaPelialueenKoko(_koko);
-   for(unsigned int y = 1; y <= _koko.haeYkoordinaatti(); y++)
-   {
-      for(unsigned int x = 1; x <= _koko.haeXkoordinaatti(); x++)
-      {
-         Julkinen::Koordinaatti koordinaatti(x, y);
-         
-         _peli->lisaaPala(maaritaPalaTyyppi(x,y),
-                          _alustaja->annaKoordinaatinRotaatio(x, y), koordinaatti);
-      }
-   }
-   if (Komentotulkki::onkoRaakileTilassa())
-   {
-      return;
-   }
-   
-   
-   for(unsigned int i = 1; i <= _koko.haeYkoordinaatti(); i++ )
-   {
-      for(unsigned int j = 1; j <= _koko.haeXkoordinaatti(); j++)
-      {
-         Julkinen::ErikoispalaTyyppi tyyppi = Julkinen::NORMAALI;
-         Julkinen::Koordinaatti kohde = Julkinen::Koordinaatti();
-         char erikoispala = _alustaja->annaKoordinaatinErikoispala(j, i);
-         if(erikoispala == 'T' || erikoispala == 'K' || erikoispala =='S')
-         {   
-            if(erikoispala == 'T')
-            {
-               tyyppi = Julkinen::TELEPORTTI;
-               kohde = _alustaja->annaSeuraavaKohde();
-            }
-            
-            else if(erikoispala == 'K')
-            {
-               tyyppi = Julkinen::KIROTTU; 
-            }
-            
-            else if(erikoispala == 'S')
-            {
-               tyyppi = Julkinen::SIUNATTU; 
-            }
-            
-            erikoispala = '*';
-         }
-         
-         Julkinen::Koordinaatti sijainti(j,i);        
-         try
-         {
-            _peli->asetaPalanTyyppi(tyyppi, sijainti, kohde);
-         }
-         catch(Julkinen::ToteuttamatonVirhe virhe)
-         {
-            std::cerr << virhe << std::endl;
-            _peli->asetaPalanTyyppi(Julkinen::NORMAALI, sijainti, kohde);
-            
-         }
-         
-      }
-         
-   }
-      
+	ESIEHTO(_alustaja != 0);
+	_peli->maaritaPelialueenKoko(_koko);
+	for (unsigned int y = 1; y <= _koko.haeYkoordinaatti(); y++)
+	{
+		for (unsigned int x = 1; x <= _koko.haeXkoordinaatti(); x++)
+		{
+			Julkinen::Koordinaatti koordinaatti(x, y);
+
+			_peli->lisaaPala(maaritaPalaTyyppi(x, y),
+				_alustaja->annaKoordinaatinRotaatio(x, y), koordinaatti);
+		}
+	}
+	if (Komentotulkki::onkoRaakileTilassa())
+	{
+		return;
+	}
+
+
+	for (unsigned int i = 1; i <= _koko.haeYkoordinaatti(); i++)
+	{
+		for (unsigned int j = 1; j <= _koko.haeXkoordinaatti(); j++)
+		{
+			Julkinen::ErikoispalaTyyppi tyyppi = Julkinen::NORMAALI;
+			Julkinen::Koordinaatti kohde = Julkinen::Koordinaatti();
+			char erikoispala = _alustaja->annaKoordinaatinErikoispala(j, i);
+			if (erikoispala == 'T' || erikoispala == 'K' || erikoispala == 'S')
+			{
+				//Harjoitustyˆss‰ kaikki erikoispalat ovat kirottuja
+				tyyppi = Julkinen::KIROTTU;
+
+				erikoispala = '*';
+			}
+
+			Julkinen::Koordinaatti sijainti(j, i);
+			try
+			{
+				_peli->asetaPalanTyyppi(tyyppi, sijainti, kohde);
+			}
+			catch (Julkinen::ToteuttamatonVirhe virhe)
+			{
+				std::cerr << virhe << std::endl;
+				_peli->asetaPalanTyyppi(Julkinen::NORMAALI, sijainti, kohde);
+
+			}
+
+		}
+
+	}
+
 }
-   
+
 
 void Rakentaja::lisaaIrtopala()
 {
@@ -186,25 +173,25 @@ void Rakentaja::lisaaIrtopala()
 void Rakentaja::luoPelaajat()
 {
 	unsigned int ihmispelaajia = 0;
-	for(unsigned int i = 0; i < ihmispelaajia; i++)
+	for (unsigned int i = 0; i < ihmispelaajia; i++)
 	{
 		PelaajaTiedot tiedot;
 		tiedot._tyyppi = Julkinen::IHMINEN;
 		tiedot._nimi = maaritaNimi();
-		tiedot._merkki = int2char(static_cast<int>(i) + 1);
+		tiedot._merkki = int2char(static_cast<int>(i)+1);
 		tiedot._sijainti = luoSijainti();
 		_pelaajat.push_back(tiedot);
 	}
 
 	unsigned int tietokonepelaajia = 0;
-	for(unsigned int i = 0; i < tietokonepelaajia; i++)
+	for (unsigned int i = 0; i < tietokonepelaajia; i++)
 	{
 		PelaajaTiedot tiedot;
 		std::string nimi = "Tietokone";
-		nimi += int2char(static_cast<int>(i) + 1);
+		nimi += int2char(static_cast<int>(i)+1);
 		tiedot._tyyppi = Julkinen::TIETOKONE;
 		tiedot._nimi = nimi;
-		tiedot._merkki = int2char(static_cast<int>(i) + 1 + static_cast<int>(ihmispelaajia));
+		tiedot._merkki = int2char(static_cast<int>(i)+1 + static_cast<int>(ihmispelaajia));
 		tiedot._sijainti = luoSijainti();
 		_pelaajat.push_back(tiedot);
 	}
@@ -214,7 +201,7 @@ void Rakentaja::luoPelaajat()
 
 void Rakentaja::lisaaPelaajat()
 {
-	for(unsigned int i = 0; i < _pelaajat.size(); i++)
+	for (unsigned int i = 0; i < _pelaajat.size(); i++)
 	{
 		_peli->lisaaPelaaja(_pelaajat[i]._tyyppi,
 			_pelaajat[i]._nimi,
@@ -228,27 +215,27 @@ void Rakentaja::lisaaPelaajat()
 
 void Rakentaja::lisaaEsineet()
 {
-    if (Komentotulkki::onkoRaakileTilassa()) { return; }
-  
-	for(unsigned int i = 1; i <= _koko.haeYkoordinaatti(); i++ )
+	if (Komentotulkki::onkoRaakileTilassa()) { return; }
+
+	for (unsigned int i = 1; i <= _koko.haeYkoordinaatti(); i++)
 	{
-		for(unsigned int j = 1; j <= _koko.haeXkoordinaatti(); j++)
+		for (unsigned int j = 1; j <= _koko.haeXkoordinaatti(); j++)
 		{
-			
+
 			char esine = _alustaja->annaKoordinaatinEsine(j, i);
-			
-			Julkinen::Koordinaatti sijainti(j,i);
+
+			Julkinen::Koordinaatti sijainti(j, i);
 			std::string pelaaja = "";
-			for(unsigned int p = 0; p < _pelaajienmaara; p++)
+			for (unsigned int p = 0; p < _pelaajienmaara; p++)
 			{
 				std::vector<char> esineet = _alustaja->annaPelaajanEsinelista(p);
-				for(unsigned int k = 0; k < esineet.size(); k++)
+				for (unsigned int k = 0; k < esineet.size(); k++)
 				{
-					if(esineet[k] == esine){ pelaaja = _pelaajat[p]._nimi; }
+					if (esineet[k] == esine){ pelaaja = _pelaajat[p]._nimi; }
 				}
 
 			}
-			if(esine != 'x')
+			if (esine != 'x')
 			{
 				_peli->lisaaEsine(esine, sijainti, pelaaja);
 			}
@@ -260,28 +247,28 @@ void Rakentaja::lisaaEsineet()
 
 
 Julkinen::PalaTyyppi
-	Rakentaja::maaritaPalaTyyppi(unsigned int x, unsigned int y) const
+Rakentaja::maaritaPalaTyyppi(unsigned int x, unsigned int y) const
 {
-	return maaritaPalaTyyppi(_alustaja->annaKoordinaatinRuututyyppi( x, y ));
+	return maaritaPalaTyyppi(_alustaja->annaKoordinaatinRuututyyppi(x, y));
 }
 
 Julkinen::PalaTyyppi
-	Rakentaja::maaritaIrtoPalanTyyppi() const
+Rakentaja::maaritaIrtoPalanTyyppi() const
 {
 	return maaritaPalaTyyppi(_alustaja->annaIrtopalanTyyppi());
 }
 
 Julkinen::PalaTyyppi
-	Rakentaja::maaritaPalaTyyppi(Alustaja::Ruututyyppi alustustyyppi) const
+Rakentaja::maaritaPalaTyyppi(Alustaja::Ruututyyppi alustustyyppi) const
 {
 	Julkinen::PalaTyyppi tyyppi = Julkinen::IPALA;
-	switch(alustustyyppi)
+	switch (alustustyyppi)
 	{
-		case Alustaja::I: tyyppi = Julkinen::IPALA;
-			break;
-		case Alustaja::T: tyyppi = Julkinen::TPALA;
-			break;
-		case Alustaja::L: tyyppi = Julkinen::LPALA;
+	case Alustaja::I: tyyppi = Julkinen::IPALA;
+		break;
+	case Alustaja::T: tyyppi = Julkinen::TPALA;
+		break;
+	case Alustaja::L: tyyppi = Julkinen::LPALA;
 	}
 
 	return tyyppi;
@@ -309,26 +296,26 @@ char Rakentaja::int2char(int a)
 Julkinen::Koordinaatti Rakentaja::luoSijainti()
 {
 	Julkinen::Koordinaatti sijainti;
-	switch(_pelaajialisatty)
+	switch (_pelaajialisatty)
 	{
 	case 0:
-		sijainti = Julkinen::Koordinaatti(1 , 1);
+		sijainti = Julkinen::Koordinaatti(1, 1);
 		break;
 	case 1:
-		sijainti = Julkinen::Koordinaatti(1 , _koko.haeYkoordinaatti());
+		sijainti = Julkinen::Koordinaatti(1, _koko.haeYkoordinaatti());
 		break;
 	case 2:
-		sijainti = Julkinen::Koordinaatti(_koko.haeXkoordinaatti() , 1);
+		sijainti = Julkinen::Koordinaatti(_koko.haeXkoordinaatti(), 1);
 		break;
 	case 3:
 		sijainti = Julkinen::Koordinaatti(_koko.haeXkoordinaatti(),
-				_koko.haeYkoordinaatti());
+			_koko.haeYkoordinaatti());
 		break;
 	}
 	return sijainti;
 }
 
-int Rakentaja::str2int (std::string const & str)  const
+int Rakentaja::str2int(std::string const & str)  const
 {
 	std::stringstream ss(str);
 	int n;
